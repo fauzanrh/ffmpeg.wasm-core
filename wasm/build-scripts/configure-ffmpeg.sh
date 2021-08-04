e2ad9d1972389163c29c8230e412bb14073c9baa
macOzan <fauzan.habib@outlook.com>
2021-08-04 07:25:15 +0700

    disable x264

--- wasm/build-scripts/configure-ffmpeg.sh
+++ wasm/build-scripts/configure-ffmpeg.sh
@@ -6,7 +6,6 @@ source $(dirname $0)/var.sh
 FLAGS=(
   "${FFMPEG_CONFIG_FLAGS_BASE[@]}"
   --enable-gpl            # required by x264
-  --enable-libx264        # enable x264
 )
 echo "FFMPEG_CONFIG_FLAGS=${FLAGS[@]}"
 emconfigure ./configure "${FLAGS[@]}"
